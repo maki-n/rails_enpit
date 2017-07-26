809b69c3dc7bc23015f21f225d9937a2d6608830
maki-n <z16081nm@aiit.ac.jp>
2017-07-26 18:59:34 +0900

    Update books_controller.rb

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,7 +1,7 @@
 class BooksController < ApplicationController
   before_action :set_book, only: [:show, :edit, :update, :destroy]
 
-  GET /books
+  # GET /books
   # GET /books.json
   def index
     @books = Book.all
